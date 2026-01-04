ff236c7382f3f4e2cb9b10d4614c7e54f58782be
mpenzani <mpenzani@gmail.com>
2026-01-04 15:50:17 -0300

    base empty root

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/raul-logistica/',
+  base: '',  // Vazio - sem /raul-logistica/
   plugins: [react()],
 })
